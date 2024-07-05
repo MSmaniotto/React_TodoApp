5e2476de29d03f33dbd33eccbeeb40dd366e22f7
Manfredi <manfri96@gmail.com>
2024-07-05 15:54:35 +0200

    first commit TodoList

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,17 +2,42 @@ module.exports = {
   root: true,
   env: { browser: true, es2020: true },
   extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:react-hooks/recommended',
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:react-hooks/recommended",
   ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
-  parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
+  ignorePatterns: ["dist", ".eslintrc.cjs"],
+  parser: "@typescript-eslint/parser",
+  plugins: ["react-refresh"],
   rules: {
-    'react-refresh/only-export-components': [
-      'warn',
+    "react-refresh/only-export-components": [
+      "warn",
       { allowConstantExport: true },
     ],
   },
-}
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+    project: ["./tsconfig.json", "./jsconfig.node.json"],
+    tsconfigRootDir: __dirname,
+  },
+  // settings: {
+  //   "import/resolver": {
+  //     node: {
+  //       extensions: [".js", ".vue", ".ts", ".d.ts"],
+  //     },
+  //     alias: {
+  //       extensions: [".js", ".ts", ".scss", ".d.ts"],
+  //       map: [
+  //         ["./components", "./src/components"],
+  //         ["./pages", "./src/pages"],
+  //         ["./router", "./src/router"],
+  //         ["./store", "./src/store"],
+  //         ["./styles", "./src/styles"],
+  //         ["./types", "./src/types"],
+  //         ["./utils", "./src/utils"],
+  //       ],
+  //     },
+  //   },
+  // },
+};
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { resolve } from "path";
+import { fileURLToPath } from 'url';
+
+const projectRootDir = resolve(__dirname!);
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      //[{find: '@', replacement: fileURLToPath(new URL('./src', import.meta.url))!}],
+      '@': resolve(projectRootDir, "src"),//npm install @types/node --save-dev
+      //find: '@', replacement: resolve(projectRootDir, 'src'),
+    },
+  },
 })
